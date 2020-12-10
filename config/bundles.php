2fa85b62e35cb2abd859d12d12a9b3655c05960c
Quentin Gans <contact@quentingans.fr>
2020-12-10 14:14:43 +0100

    ✨ Add JWT refresh_token

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
